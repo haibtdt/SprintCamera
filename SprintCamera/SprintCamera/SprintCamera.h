6c5501552410806f36d5da4cdd2f32e82d7f3dc6
Hai Bui <haibui19872015@gmail.com>
2015-09-04 17:27:09 +0700

    Initial commit

---
+++ SprintCamera/SprintCamera/SprintCamera.h
@@ -0,0 +1,19 @@
+//
+//  SprintCamera.h
+//  SprintCamera
+//
+//  Created by SB 8 on 9/4/15.
+//  Copyright (c) 2015 vn.haibui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SprintCamera.
+FOUNDATION_EXPORT double SprintCameraVersionNumber;
+
+//! Project version string for SprintCamera.
+FOUNDATION_EXPORT const unsigned char SprintCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SprintCamera/PublicHeader.h>
+
+
